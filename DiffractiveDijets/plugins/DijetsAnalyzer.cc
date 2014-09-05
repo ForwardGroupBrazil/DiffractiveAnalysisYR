d814ac1d7ea55a8d56d2e180a3cf86b88ac0dd44
Diego Figueiredo <dmf@cern.ch>
2014-09-05 15:28:30 +0200

    Update DijetsAnalyzer.cc

--- DiffractiveDijets/plugins/DijetsAnalyzer.cc
+++ DiffractiveDijets/plugins/DijetsAnalyzer.cc
@@ -318,7 +318,7 @@ void DijetsAnalyzer::FillCollections(const edm::Event& iEvent, const edm::EventS
     for(itGen=0; itGen < gensize; ++itGen){
       const reco::GenParticle* genAll = &((*genParticle)[itGen]);
       if (genAll->status() != 1) continue;
-      if (genAll->pdgId() != 2212) continue;
+      if (fabs(genAll->pdgId()) != 2212) continue;
       protonVector.push_back(genAll);
     }
   }
